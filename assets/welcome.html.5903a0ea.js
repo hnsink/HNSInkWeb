f9cd54dfe12b4b5260c6b6bd2356030dd0e8a631
charles98jones <charles.jones.98@mail.ru>
2022-04-06 12:43:27 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.5903a0ea.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to HNS.Ink",lang:"en-US",frontmatter:{index:1,title:"Welcome to HNS.Ink",author:"HNS.Ink",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`HNS Ink : Humaned Naming Service Ink.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://hns.ink/welcome.html"}],["meta",{property:"og:title",content:"Welcome to HNS.Ink"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"HNS.Ink"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>HNS Ink : Humaned Naming Service Ink.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"HNS Ink",slug:"hns-ink",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why HNSInk?",slug:"why-hnsink",children:[]},{level:2,title:"Tech Stack to Build HNS Ink",slug:"tech-stack-to-build-hns-ink",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649220141e3,updatedTime:1649220141e3,contributors:[{name:"charles98jones",email:"charles.jones.98@mail.ru",commits:1}]},readingTime:{minutes:.45,words:134},filePathRelative:"welcome.md"};export{e as data};
